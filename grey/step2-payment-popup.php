98954f35a7ba25317096c354d0401144aab147d1
Emine Shaban <emineshaban@icloud.com>
2024-01-31 13:56:29 +0200

    .

--- grey/step2-payment-popup.php
+++
@@ -1,5 +0,0 @@
-<?php include_once '/home/campaign/campaign/active/cdn/ayra/inc/header-light-b.php';?>
-		<!-- BEGIN FORM -->
-			<?php include_once '/home/campaign/campaign/active/cdn/forms/ayra/form-payment-popup.html';?>
-		<!-- END FORM -->
-<?php include_once '/home/campaign/campaign/active/cdn/ayra/inc/footer-light.php';?>
--- grey/step2.php
+++
@@ -1,5 +0,0 @@
-<?php include_once '/home/campaign/campaign/active/cdn/ayra/inc/header-light-b.php';?>
-		<!-- BEGIN FORM -->
-			<?php include_once '/home/campaign/campaign/active/cdn/forms/ayra/form.html';?>
-		<!-- END FORM -->
-<?php include_once '/home/campaign/campaign/active/cdn/ayra/inc/footer-light.php';?>
