f466d318e11a5df46b11276da89e260d982395ec
Emine Shaban <emineshaban@icloud.com>
2024-01-31 14:31:39 +0200

    .

--- js/country-popup.js
+++ js/country-popup.js
@@ -1,11 +1,13 @@
-myButton.addEventListener("click", function () {
-    myPopup.classList.add("show");
-});
-closePopup.addEventListener("click", function () {
-    myPopup.classList.remove("show");
-});
-window.addEventListener("click", function (event) {
-    if (event.target == myPopup) {
-        myPopup.classList.remove("show");
-    }
+ 
+
+
+let changeCountry = document.getElementById('change-country')
+let country1 = document.getElementById('country1')
+let country2 = document.getElementById('country2')
+
+changeCountry.addEventListener("click", function (e) {
+ e.preventDefault()
+    country1.style.visibility = 'hidden';
+    country2.style.visibility = 'visible';
+
 });
