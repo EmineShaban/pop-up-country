bb72cfb94f41713bb371208358d66822c53577ea
Emine Shaban <emineshaban@icloud.com>
2024-02-01 08:44:15 +0200

    .

--- js/country-popup.js
+++ js/country-popup.js
@@ -1,13 +1,24 @@
- 
-
-
-let changeCountry = document.getElementById('change-country')
-let country1 = document.getElementById('country1')
-let country2 = document.getElementById('country2')
+let changeCountry = document.getElementById("change-country");
+let country = document.getElementsByClassName("country-img");
+let country1 = document.getElementById("country1");
+let country2 = document.getElementById("country2");
 
 changeCountry.addEventListener("click", function (e) {
- e.preventDefault()
-    country1.style.visibility = 'hidden';
-    country2.style.visibility = 'visible';
-
+  e.preventDefault();
+  country1.style.visibility = "hidden";
+  country2.style.visibility = "visible";
 });
+
+const buttonPressed = (e) => {
+  e.preventDefault();
+  console.log(e.target.parentElement.className);
+  if (e.target.parentElement.className == "country-img") {
+    e.target.parentElement.className += " border";
+  } else {
+    e.target.parentElement.className = "country-img";
+  }
+};
+
+for (let index = 0; index < country.length; index++) {
+  country[index].addEventListener("click", buttonPressed);
+}
--- js/ham.js
+++
@@ -1,4 +0,0 @@
-$('.navbar-icon').click(function () {
-    $(this).toggleClass("active");
-    $(".navbar-menu nav").toggleClass("active");
-});
